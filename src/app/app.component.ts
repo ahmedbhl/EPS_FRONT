5a6941cde41b65ca658759e5df6676192662ff66
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-07-14 01:07:32 +0200

    add the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,20 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class AppComponent {
   options: FormGroup;
 
-  constructor(fb: FormBuilder) {
+  constructor(private readonly matIconRegistry: MatIconRegistry,
+    private readonly domSanitizer: DomSanitizer, fb: FormBuilder) {
     this.options = fb.group({
       bottom: 0,
       fixed: false,
       top: 0
     });
+
+    // Define the path of external icons
+    this.matIconRegistry.addSvgIcon('facebook', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/social/facebook.svg'));
+    this.matIconRegistry.addSvgIcon('youtube', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/social/youtube.svg'));
+    this.matIconRegistry.addSvgIcon('linkedin', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/social/linkedin.svg'));
+    this.matIconRegistry.addSvgIcon('twitter', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/social/twitter.svg'));
+    this.matIconRegistry.addSvgIcon('eps', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/social/eps50.svg'));
   }
 
 }
