e7b8c37a8acedcc7536865bd3dac119b8ca0f381
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-07-25 02:26:06 +0200

    fix the user access permission

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'login', loadChildren: './modules/login/login.module#LoginModule' },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadChildren: './modules/home/home.module#HomeModule' },
+    { path: 'login', loadChildren: './modules/login/login.module#LoginModule' },
     { path: 'homeActors', loadChildren: './modules/home-actors/home-actors.module#HomeActorsModule' }
 ];
