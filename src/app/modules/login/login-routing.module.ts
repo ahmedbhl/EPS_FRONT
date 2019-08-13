49c2def1e421843ee7fde89b7dc8a8d3663fadd8
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-08-13 23:06:21 +0200

    firebase config

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -26,6 +26,6 @@ export class LoginRoutingModule { }
 export function userAddRedirectTo() {
   console.log('Access Denied - insufficient rights for this function');
   // Helper.broadcast('Access Denied - insufficient rights for this function');
-  return '404';
+  return '/error500';
 }
 
