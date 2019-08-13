edd209c0dca00e09c8549bbe7873966bd6add55e
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-08-14 00:08:05 +0200

    add api urls for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+
+  users_api_url: 'http://192.168.56.1:8080/api/v1/users',
+  users_api_backend_type: 'java',
+
+  educational_institution_url: 'http://192.168.56.1:8080/api/v1/establishments',
+  educational_institution_api_backend_type: 'java',
+
+  /** Environment platform name */
+  name: 'Prod',
+
+  /** Activate Logs */
+  log: 'true'
 };
