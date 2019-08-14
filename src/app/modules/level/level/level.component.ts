0205bba704a0909c12698f8a4af66a1764b086f1
Ahmed BOUHLEL <ase.bouhlel@gmail.com>
2019-08-14 04:18:33 +0200

    add the level service

--- src/app/modules/level/level/level.component.ts
+++ src/app/modules/level/level/level.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { Helper } from 'src/app/core/helper.service';
+import { Level } from '../model/level';
+import { LevelService } from '../services/level.service';
 
 @Component({
   selector: 'app-level',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LevelComponent implements OnInit {
 
-  constructor() { }
+  levels: Level[] = [];
+
+  displayedColumns: string[] = ['id', 'levelName', 'description'];
+  dataSource: MatTableDataSource<Level>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+  constructor(private readonly helper: Helper,
+    private readonly _LevelService: LevelService,
+    public dialog: MatDialog) {
+  }
 
   ngOnInit() {
+    this.getAllLevel();
   }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  /**
+  * Get all Levels
+  */
+  getAllLevel() {
+    this._LevelService.getAllLevels().subscribe(
+      (data: Level[]) => {
+        this.levels = data;
+        // Assign the data to the data source for the table to render
+        this.dataSource = new MatTableDataSource(this.levels);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+      ,
+      error => this.helper.handleError,
+      () => this.helper.trace('Get all Levels complete ' + this.levels.length));
+  }
+
+  /* openDialog(): void {
+    const dialogRef = this.dialog.open(LevelModalComponent, {
+      width: '250px',
+      data: { name: 'Guest', animal: 'Guest' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.helper.trace('The dialog was closed' + result);
+    });
+  }*/
 
 }
